32541ca7173364c607c0e087fce6df41149bd7ee
Beckah Moscuzza <rhmoscuzza@gmail.com>
2018-02-14 12:01:10 -0500

    began building js

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,4 +1,22 @@
 import React from "react";
 import ReactDOM from "react-Dom";
 
-console.log('setup complete')
+var dNumber = [4, 6, 8, 10, 12,	20];
+
+const Dice = (amount) => {
+	return (
+		<button value={amount.value} className='dice' onClick={amount.update}>
+		</button>
+	)
+};
+const Times = (amount) => {
+	return (
+		<input className='times' type='number' min='1' max='10' placeholder='1' onChange={amount.update}></input>
+	)
+};
+const Final = (amount) => {
+	return (
+		<h2 className='final'>{amount.final}</h2>
+	)
+};
+
