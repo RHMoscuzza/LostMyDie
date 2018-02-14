ead642e3225e1520fac6ee24a01cd475b6dda3a3
Beckah Moscuzza <rhmoscuzza@gmail.com>
2018-02-14 13:07:51 -0500

    finished base JS started the redering

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,22 +1,38 @@
+// Import React from node_modules
 import React from "react";
 import ReactDOM from "react-Dom";
 
+// Dice Number Array (d4, d6...)
 var dNumber = [4, 6, 8, 10, 12,	20];
 
+// Input how many dice you are rolling
+const Times = (amount) => {
+	return (
+		<input className='times' type='number' min='1' max='10' placeholder='1' onChange={amount.update}></input>
+	)
+};
+// Select your die
 const Dice = (amount) => {
 	return (
 		<button value={amount.value} className='dice' onClick={amount.update}>
 		</button>
 	)
 };
-const Times = (amount) => {
-	return (
-		<input className='times' type='number' min='1' max='10' placeholder='1' onChange={amount.update}></input>
-	)
-};
+// The final result of the base math
 const Final = (amount) => {
 	return (
 		<h2 className='final'>{amount.final}</h2>
 	)
 };
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			times: 1,
+			final: 0
+		};
+		this.newTimes = this.newTimes.bind(this);
+		this.findFinal = this.findFinal.bind(this);
+	}
 
+}
